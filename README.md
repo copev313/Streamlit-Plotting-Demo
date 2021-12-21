9e5b4c21d49064f35720afd53d0fa8025182a5de
Evan Cope <copev313@gmail.com>
2021-12-21 15:16:05 -0500

    added plotting demo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Streamlit-Plotting-Demo
  
+ A simple demonstration of Streamlit plotting capabilities.
+
+---
+## Getting Started
+
+1. Install Streamlit:
+
+    ```pip install streamlit```
+
+2. Run the app:
+
+    ```streamlit run plot_demo.py```
+
+3. Open the app in your browser and enjoy!
+
+    Local URL: [http://localhost:8501](http://localhost:8501)
---
+++ plotting_app/plot_demo.py
@@ -0,0 +1,31 @@
+"""
+plot_demo.py
+
+A simple demonstration of Streamlit plotting capabilities.
+"""
+import streamlit as st
+from time import sleep
+import numpy as np
+
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text(f"{i}% Complete")
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    sleep(0.05)
+
+progress_bar.empty()
+
+
+# Our Streamlit widgets automatically run the script from top to bottom.
+# Since this button is not connected to any other logic, it will just trigger
+# a plain re-run.
+st.button("Re-run!")
